6437b3e4ba9bc12d9c0c61bb86ae973e92075c67
CharlesHMiranda <65673681+CharlesHMiranda@users.noreply.github.com>
2020-06-17 17:14:41 -0300

    Segundo commit

---
+++ js/aula.js
@@ -0,0 +1,14 @@
+
+
+const arr = [1, 2, 3, 4, 5];
+
+arrReduced = arr.reduce((total, value) => total += value, 0);
+// 15
+
+console.log(arrReduced);
+
+const persons = Array.of('John', 'Cris', 'Jenny');
+
+console.log("Passei por aqui.");
+console.log(persons);
+
